c0a2a03ac095451e6387859b4c6534328923b3f6
Christian Abdelmassih <chrabd@kth.se>
2016-04-18 17:42:08 +0200

    Printing

--- calculate.py
+++ calculate.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
-import datetime 
-import numpy as np 
+import datetime
+import numpy as np
 
 def get_tweets_from_file(filename):
 	with open(filename, "r") as f:
@@ -14,26 +14,26 @@ def get_tweets_from_file(filename):
 
 def tweet_interval_avg(dateObj, num_days, tweet_objects):
     """Takes a datetime object, an int and a list of tweetobjecs and returns the average score
-    for tweets within that span. 
+    for tweets within that span.
 
     Example dateOjb: dateObj = datetime.datetime.strptime("2015-06-01 00:00:00", "%Y-%m-%d %H:%M:%S") """
 
     span = datetime.timedelta(days= num_days).total_seconds()
-    #print("SPAN: ", datetime.timedelta(days= num_days), 
+    #print("SPAN: ", datetime.timedelta(days= num_days),
     #    "Seconds:", datetime.timedelta(days= num_days).total_seconds())
 
     total_score = 0
     hits = 0
-    for items in tweet_objects:
-        #print((items["date"] - dateObj))
-        if (abs(items["date"] - dateObj).total_seconds()) < span:
-            #print("Hit within span, score: ", items["score"])
-            total_score += items["score"]
+    for tweet in tweet_objects:
+        #print((tweet["date"] - dateObj))
+        if (abs(tweet["date"] - dateObj).total_seconds()) < span:
+            #print("Hit within span, score: ", tweet["score"])
+            total_score += tweet["score"]
             hits += 1
 
     if hits == 0:
         return None
-    else: 
+    else:
         return total_score / hits
 
 def tweets_per_golfer(tweets):
@@ -50,7 +50,7 @@ def tweets_per_golfer(tweets):
     return golf_list
 
 def get_stats_from_file(filename):
-    stat_objects = [] 
+    stat_objects = []
     with open(filename, "r") as f:
         stats = f.readlines()
 
@@ -65,40 +65,40 @@ def create_competition_stat_object():
     pga_northerntrust_2015 = {"name": "pga_northerntrust_2015", "date": ["2015-02-16", "2015-02-22"]}
     pga_ohlclassic_2015 = {"name": "pga_ohlclassic_2015", "date": ["2014-11-10", "2014-11-16"]}
     pga_pebblebeach_2015 = {"name": "pga_pebblebeach_2015", "date": ["2015-02-09", "2015-02-15"]}
-    pga_arnoldpalmer_2015 = {"name": "pga_arnoldpalmer_2015", "date": ["2015-03-16", "2015-03-22"]}          
+    pga_arnoldpalmer_2015 = {"name": "pga_arnoldpalmer_2015", "date": ["2015-03-16", "2015-03-22"]}
     pga_pgachampionship_2015 = {"name": "pga_pgachampionship_2015", "date": ["2015-08-10", "2015-08-16"]}
-    pga_barclays_2015 = {"name": "pga_barclays_2015", "date": ["2015-08-24", "2015-08-30"]}              
+    pga_barclays_2015 = {"name": "pga_barclays_2015", "date": ["2015-08-24", "2015-08-30"]}
     pga_playerschampions_2015 = {"name": "pga_playerschampions_2015", "date": ["2015-05-04", "2015-05-10"]}
-    pga_bmw_2015 = {"name": "pga_bmw_2015", "date": ["2015-09-14", "2015-09-20"]}                   
+    pga_bmw_2015 = {"name": "pga_bmw_2015", "date": ["2015-09-14", "2015-09-20"]}
     pga_puertoricoopen_2015 = {"name": "pga_puertoricoopen_2015", "date": ["2015-03-02", "2015-03-08"]}
-    pga_bridgestone_2015 = {"name": "pga_bridgestone_2015", "date": ["2015-08-03", "2015-08-09"]}           
+    pga_bridgestone_2015 = {"name": "pga_bridgestone_2015", "date": ["2015-08-03", "2015-08-09"]}
     pga_quickenloans_2015 = {"name": "pga_quickenloans_2015", "date": ["2015-07-27", "2015-08-02"]}
-    pga_cadillacchampionship_2015 = {"name": "pga_cadillacchampionship_2015", "date": ["2015-03-02", "2015-03-08"]}  
+    pga_cadillacchampionship_2015 = {"name": "pga_cadillacchampionship_2015", "date": ["2015-03-02", "2015-03-08"]}
     pga_rbccanadianopen_2015 = {"name": "pga_rbccanadianopen_2015", "date": ["2015-07-20", "2015-07-26"]}
-    pga_cimbclassic_2015 = {"name": "pga_cimbclassic_2015", "date": ["2014-10-20", "2014-10-26"]}           
+    pga_cimbclassic_2015 = {"name": "pga_cimbclassic_2015", "date": ["2014-10-20", "2014-10-26"]}
     pga_rbcheritage_2015 = {"name": "pga_rbcheritage_2015", "date": ["2015-04-13", "2015-04-19"]}
     pga_farmersinsurance_2015 = {"name": "pga_farmersinsurance_2015", "date": ["2015-02-02", "2015-02-08"]}
-    pga_cocacola_2015 = {"name": "pga_cocacola_2015", "date": ["2015-09-21", "2015-09-27"]}            
+    pga_cocacola_2015 = {"name": "pga_cocacola_2015", "date": ["2015-09-21", "2015-09-27"]}
     pga_sanderson_2015 = {"name": "pga_sanderson_2015", "date": ["2014-11-03", "2014-11-09"]}
-    pga_deutchebank_2015 = {"name": "pga_deutchebank_2015", "date": ["2015-08-31", "2015-09-07"]}           
+    pga_deutchebank_2015 = {"name": "pga_deutchebank_2015", "date": ["2015-08-31", "2015-09-07"]}
     pga_shriners_2015 = {"name": "pga_shriners_2015", "date": ["2014-10-13", "2014-10-19"]}
-    pga_frys_2015 = {"name": "pga_frys_2015", "date": ["2014-10-06", "2014-10-12"]}                  
+    pga_frys_2015 = {"name": "pga_frys_2015", "date": ["2014-10-06", "2014-10-12"]}
     pga_sonyopen_2015 = {"name": "pga_sonyopen_2015", "date": ["2015-01-12", "2015-01-18"]}
-    pga_greenbrier_2015 = {"name": "pga_greenbrier_2015", "date": ["2015-06-29", "2015-07-05"]}            
+    pga_greenbrier_2015 = {"name": "pga_greenbrier_2015", "date": ["2015-06-29", "2015-07-05"]}
     pga_thememorial_2015 = {"name": "pga_thememorial_2015", "date": ["2015-06-01", "2015-06-07"]}
-    pga_hondaclassic_2015 = {"name": "pga_hondaclassic_2015", "date": ["2015-02-23", "2015-03-01"]}          
+    pga_hondaclassic_2015 = {"name": "pga_hondaclassic_2015", "date": ["2015-02-23", "2015-03-01"]}
     pga_theopen_2015 = {"name": "pga_theopen_2015", "date": ["2015-07-13", "2015-07-19"]}
-    pga_houstonopen_2015 = {"name": "pga_houstonopen_2015", "date": ["2015-03-30", "2015-04-05"]}           
+    pga_houstonopen_2015 = {"name": "pga_houstonopen_2015", "date": ["2015-03-30", "2015-04-05"]}
     pga_usopen_2015 = {"name": "pga_usopen_2015", "date": ["2015-06-15", "2015-06-21"]}
-    pga_hsbcchampions_2015 = {"name": "pga_hsbcchampions_2015", "date": ["2014-11-03", "2014-11-09"]}         
+    pga_hsbcchampions_2015 = {"name": "pga_hsbcchampions_2015", "date": ["2014-11-03", "2014-11-09"]}
     pga_valspar_2015 = {"name": "pga_valspar_2015", "date": ["2015-03-09", "2015-03-15"]}
-    pga_humana_2015 = {"name": "pga_humana_2015", "date": ["2015-01-19", "2015-01-25"]}                
+    pga_humana_2015 = {"name": "pga_humana_2015", "date": ["2015-01-19", "2015-01-25"]}
     pga_wastemanagement_2015 = {"name": "pga_wastemanagement_2015", "date": ["2015-01-26", "2015-02-01"]}
-    pga_hyundai_2015 = {"name": "pga_hyundai_2015", "date": ["2015-01-05", "2015-01-12"]}               
+    pga_hyundai_2015 = {"name": "pga_hyundai_2015", "date": ["2015-01-05", "2015-01-12"]}
     pga_wellsfargo_2015 = {"name": "pga_wellsfargo_2015", "date": ["2015-05-11", "2015-05-17"]}
-    pga_johndeere_2015 = {"name": "pga_johndeere_2015", "date": ["2015-07-06", "2015-07-12"]}             
+    pga_johndeere_2015 = {"name": "pga_johndeere_2015", "date": ["2015-07-06", "2015-07-12"]}
     pga_wyndham_2015 = {"name": "pga_wyndham_2015", "date": ["2015-08-17", "2015-08-23"]}
-    pga_masters_2015 = {"name": "pga_masters_2015", "date": ["2015-04-16", "2015-04-12"]}               
+    pga_masters_2015 = {"name": "pga_masters_2015", "date": ["2015-04-16", "2015-04-12"]}
     pga_zurich_2015 = {"name": "pga_zurich_2015", "date": ["2015-04-20", "2015-04-26"]}
     pga_valero_2015 = {"name": "pga_valero_2015", "date": ["2015-03-23", "2015-03-29"]}
     pga_crowneplaza_2015 = {"name": "pga_crowneplaza_2015", "date": ["2015-05-18", "2015-05-24"]}
@@ -135,6 +135,7 @@ def make_z_scores(stat_objects):
 
     z_scores = []
 
+    print(stat_objects)
     for i, score in enumerate(scores):
         z = calc_zscore(score, mean, std)
         z_scores.append(z)
@@ -145,7 +146,8 @@ def make_z_scores(stat_objects):
     return stat_objects, mean, z_mean, std
 
 def calc_zscore(x, mean, std):
-    z = (x - mean) / std 
+    z = (x - mean) / std
+    print("x",x,"mean", mean, "std", std, "z", z)
     return z
 
 def make_individual_stats(competitions):
@@ -216,48 +218,48 @@ def error_handler(name, golfer_id):
     elif int(golfer_id) == 2575022059:
         return sum(player_stats["Mike Weir"]) / len(player_stats["Mike Weir"])
     elif int(golfer_id) == 2980287125:
-        return sum(player_stats["Fabian Gomez"]) / len(player_stats["Fabian Gomez"])    
+        return sum(player_stats["Fabian Gomez"]) / len(player_stats["Fabian Gomez"])
     elif int(golfer_id) == 2586530467:
-        return sum(player_stats["Matt Jones"]) / len(player_stats["Matt Jones"])    
+        return sum(player_stats["Matt Jones"]) / len(player_stats["Matt Jones"])
     elif int(golfer_id) == 37843534:
-        return sum(player_stats["J.J. Henry"]) / len(player_stats["J.J. Henry"])    
+        return sum(player_stats["J.J. Henry"]) / len(player_stats["J.J. Henry"])
     elif int(golfer_id) == 72758634:
         print("No official stats for Y.E. Yang (same on PGA Tour.com as far as I can tell)")
-        return None    
+        return None
     elif int(golfer_id) == 74791999:
-        return sum(player_stats["Bubba Watson"]) / len(player_stats["Bubba Watson"])    
+        return sum(player_stats["Bubba Watson"]) / len(player_stats["Bubba Watson"])
     elif int(golfer_id) == 174596509:
-        return sum(player_stats["Sangmoon Bae"]) / len(player_stats["Sangmoon Bae"])        
+        return sum(player_stats["Sangmoon Bae"]) / len(player_stats["Sangmoon Bae"])
     elif int(golfer_id) == 180081562:
-        return sum(player_stats["Seung-Yul Noh"]) / len(player_stats["Seung-Yul Noh"])         
+        return sum(player_stats["Seung-Yul Noh"]) / len(player_stats["Seung-Yul Noh"])
     elif int(golfer_id) == 188039706:
-        return sum(player_stats["Rory McIlroy"]) / len(player_stats["Rory McIlroy"])         
+        return sum(player_stats["Rory McIlroy"]) / len(player_stats["Rory McIlroy"])
     elif int(golfer_id) == 244768359:
-        return sum(player_stats["Adam Hadwin"]) / len(player_stats["Adam Hadwin"])       
+        return sum(player_stats["Adam Hadwin"]) / len(player_stats["Adam Hadwin"])
     elif int(golfer_id) == 283052420:
-        return sum(player_stats["Michael Putnam"]) / len(player_stats["Michael Putnam"])       
+        return sum(player_stats["Michael Putnam"]) / len(player_stats["Michael Putnam"])
     elif int(golfer_id) == 306549170:
-        return sum(player_stats["Chris Stroud"]) / len(player_stats["Chris Stroud"])       
+        return sum(player_stats["Chris Stroud"]) / len(player_stats["Chris Stroud"])
     elif int(golfer_id) == 342791510:
-        return sum(player_stats["Max Homa"]) / len(player_stats["Max Homa"])       
+        return sum(player_stats["Max Homa"]) / len(player_stats["Max Homa"])
     elif int(golfer_id) == 373511205:
-        return sum(player_stats["Marc Leishman"]) / len(player_stats["Marc Leishman"])       
+        return sum(player_stats["Marc Leishman"]) / len(player_stats["Marc Leishman"])
     elif int(golfer_id) == 382202772:
-        return sum(player_stats["Michael Kim"]) / len(player_stats["Michael Kim"])       
+        return sum(player_stats["Michael Kim"]) / len(player_stats["Michael Kim"])
     elif int(golfer_id) == 558220594:
-        return sum(player_stats["Will MacKenzie"]) / len(player_stats["Will MacKenzie"])       
+        return sum(player_stats["Will MacKenzie"]) / len(player_stats["Will MacKenzie"])
     elif int(golfer_id) == 1138456724:
-        return sum(player_stats["Jason Kokrak"]) / len(player_stats["Jason Kokrak"])       
+        return sum(player_stats["Jason Kokrak"]) / len(player_stats["Jason Kokrak"])
     elif int(golfer_id) == 1924882021:
-        return sum(player_stats["Andrew Svoboda"]) / len(player_stats["Andrew Svoboda"])       
+        return sum(player_stats["Andrew Svoboda"]) / len(player_stats["Andrew Svoboda"])
     elif int(golfer_id) == 2790755710:
-        return sum(player_stats["Tony Fina"]) / len(player_stats["Tony Fina"])       
+        return sum(player_stats["Tony Fina"]) / len(player_stats["Tony Fina"])
     elif int(golfer_id) == 174780495:
-        print("No official stats for Thorbjorn Olesen (same on PGA Tour.com as far as I can tell)")  
-        return None    
+        print("No official stats for Thorbjorn Olesen (same on PGA Tour.com as far as I can tell)")
+        return None
     elif int(golfer_id) == 1152096636:
-        print("No official stats for DJ Trahan found")  
-        return None    
+        print("No official stats for DJ Trahan found")
+        return None
     else:
         raise KeyError(name + " ID: " + golfer_id)
 
@@ -266,7 +268,7 @@ def check_performance_vs_tweets(tweets, competitions, player_stats, write = Fals
     with open("golferID.txt") as f:
         golferIDs = f.read().splitlines()
     data_pairs = []
-    #TODO: Make into a loop for all golfers! 
+    #TODO: Make into a loop for all golfers!
     for golfer in golferIDs:
         golfer_tweets = []
         golfer_afinnscore = []
@@ -292,11 +294,11 @@ def check_performance_vs_tweets(tweets, competitions, player_stats, write = Fals
             interval_avg = tweet_interval_avg(date, 3, golfer_tweets)
             if interval_avg != None:
                 afinn_diff = interval_avg - golfer_afinn_mean #VARIABLE
-            else: 
+            else:
                 afinn_diff = None
             """ Get golfers Z-score for competition"""
             for stat in competition["player_stats"]:
-                try: 
+                try:
                     if stat["name"] == golfer_name:
                         score_diff = stat["z-score"] - golfer_mean_z
                 except KeyError:
@@ -327,16 +329,3 @@ if __name__ == '__main__':
     player_stats, competitions = make_competition_stats(write = False)
 
     check_performance_vs_tweets(tweets, competitions, player_stats, write = False)
-
-
-
-
-
-
-
-
-
-
-
-
-
